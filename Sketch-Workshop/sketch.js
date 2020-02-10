d061e4adae94a825f941a7b5ed804b4c980f495e
JordanHofer92 <jhofer@ualberta.ca>
2020-02-10 15:39:20 -0700

    Got mousePressed to work

--- Sketch-Workshop/sketch.js
+++ Sketch-Workshop/sketch.js
@@ -1,24 +1,32 @@
 var moving = true
 var name = 'Jordan'
-var position=100
+var position = 100
+let pressed = "pressed the button"
 //Creates html5 Canvas to draw on//
 function setup() {
     //determines dimensions on html5 canvas//
     createCanvas(400, 200)
 }
 //Determines movement of small box on canvas//
+
 function draw() {
+    //background color//
     background(200)
-    position = position + 1
-    rect(position, 0, 10, 10)
+    //position/movement of small square//
+    rect(position, 00, 10, 10)
+    //if value is true//
+    if (moving) {
+        console.log(position, "hellowthrerer")
+        position = position + 1
+    }
+
     // console.log(' The position is ' + position)
     text('My name is ' + name, 10, 30)
     // console.log('The box is '+ moving)
 }
 function mousePressed() {
-    console.log(pressed)
+    moving = !moving
+    // console.log("I CLICKED JUST FINE")
+    
 }
 
-  if (moving) {
-    
-  }
